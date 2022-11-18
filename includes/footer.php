ae1a5cd7e6fa4b9136fc4ca0c70e75964cb8480e
Bolu Akinlawon <78508788+BoluAkinlawon@users.noreply.github.com>
2022-11-18 20:34:59 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+     <title>Child Care Information System</title>
+	   <link rel ="stylesheet" type ="text/css" href ="style.css">
+	   <link rel ="shortcut-icon" href ="images/logo.ico" />
+  </head>
+<center>
+  <div class ="footer"  style ="height:30px; background-color:lightblue; width:100%;" align ="center">
+  @Copyright 
+  </div>
+  </center>
+  
+  <div style ="height:3px; background-color:white; width:80%; "></div>
+  
+</html>

ab935370085ac3cd870245e88de7d816f7a43111
Bolu Akinlawon <78508788+BoluAkinlawon@users.noreply.github.com>
2022-11-18 20:41:05 +0000

    Add files via upload

---
+++ supervisor/includes/header.php
@@ -0,0 +1,37 @@
+
+<html>
+  <head>
+     <title>Electronic Logbook System</title>
+	   <link rel ="stylesheet" type ="text/css" href ="style.css">
+	   <link rel ="shortcut-icon" href ="../images/logo2.ico" />
+  </head>
+  
+  <body>
+  <div style ="height:3px; background-color:white; width:80%;"></div>
+  <center>
+  <div style ="height:5px; background-color:lightblue; width:80%;"></div>
+  </center>
+  <div align ="center">
+  <img style ="height:80px; height:80px;" align ="center" src ="images/logo2.jpg">
+  </div>
+  </body>
+  
+  <center>
+  <div style ="height:50px; background-color:lightblue; width:95%;" align ="center">
+
+  
+ <a href ="../index.php" style ="background-color: lightblue;
+  color: white;padding: 10px 20px; text-align: center;text-decoration: none;
+  display: inline-block;">Home</a>
+  
+  <a href ="../supervisor/dashboard.php" style ="background-color: gray;
+  color: white;padding: 10px 20px; text-align: center;text-decoration: none;
+  display: inline-block;">Supervisor Login</a> 
+  
+  <a href ="../student/dashboard.php" style ="background-color: lightgreen;
+  color: white;padding: 10px 20px; text-align: center;text-decoration: none;
+  display: inline-block;">Student Login</a></li>
+    
+    </ul>
+  </div>
+    <div style ="height:60px; background-color:white; width:95%;" align ="center">
